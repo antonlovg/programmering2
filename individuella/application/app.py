3fecef5d99b74303e0e25ffc033de2993763606b
antonlovg <karlanton.lovgren@gmail.com>
2023-10-24 21:30:07 +0200

    Påbärat api

--- individuella/application/app.py
+++ individuella/application/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 import ssl
+import anton
 
 app = Flask(__name__)
+
+CLIENT_ID = f"{anton.client_id}"
+CLIENT_SECRET = f"{anton.client_secret}"
 
 
 @app.route("/")
@@ -10,7 +14,7 @@ def index():
     Framsida, ska via IP ta fram användarens loc och visa nya låtar via spotifys API.
     """
 
-    return render_template('index.html', title="Testar Title")
+    return render_template('index.html', title="Home")
 
 
 @app.route("/recommendations")
@@ -20,7 +24,7 @@ def rec():
     """
 
     context = ssl._create_unverified_context()
-    return render_template('recommendations.html')
+    return render_template('recommendations.html', title="Recommendations")
 
 
 @app.route("/new")
@@ -31,11 +35,14 @@ def new():
 
     context = ssl._create_unverified_context()
 
-    return render_template('new.html')
+    return render_template('new.html', title="New releases")
 
 
 @app.route("/api")
 def api():
+
+
+
     return render_template('index.html')
 
 
