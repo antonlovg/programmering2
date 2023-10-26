4544a6d8b98bee13fe1282ea426d4a6e7b02146b
antonlovg <karlanton.lovgren@gmail.com>
2023-10-26 15:03:33 +0200

    Ändringar under skoltid

--- gruppuppgift/application/app.py
+++ gruppuppgift/application/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
 
+"""
+Att göra:
+1. Loopa igenom package.json (språkkoder)
+2. Lägga in språkkoder i dropdown tex. Svenska = sv
+3. Funktioner för search och författare
+
+.
+
+"""
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
